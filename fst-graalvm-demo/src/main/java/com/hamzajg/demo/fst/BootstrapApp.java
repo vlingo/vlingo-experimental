25a9064b55ff56bdbebdcb036d5a470dc3be427b
Hamza JGUERIM <hamzajg@gmail.com>
2021-05-03 23:27:56 +0100

    Add fst-graalvm-demo..

---
+++ fst-graalvm-demo/src/main/java/com/hamzajg/demo/fst/BootstrapApp.java
@@ -0,0 +1,11 @@
+package com.hamzajg.demo.fst;
+
+import org.nustaq.serialization.FSTConfiguration;
+
+public class BootstrapApp {
+    public static void main(String... args) {
+        final FSTConfiguration conf = FSTConfiguration.createDefaultConfiguration();
+        
+        System.out.println("Hello Demo...");
+    }
+}
